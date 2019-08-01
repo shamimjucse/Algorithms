0fe81ecdaad6f46db5b7aa345c86acdd7abb4816
Shamim Imtiaz <shamimimtiaz9@gmail.com>
2019-08-01 23:38:47 +0600

    Update CHT.cpp

--- CHT.cpp
+++ CHT.cpp
@@ -224,3 +224,94 @@ int main()
  */
 
 /************************************************/
+
+///Online update and online query
+///everything is fine as long as you want the minimum
+
+///https://codeforces.com/problemset/problem/1083/E
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+typedef long double float128;
+const ll is_query = -(1LL<<62), inf = 1e18;
+struct Line
+{
+    ll m, b;
+    mutable function<const Line*()> succ;
+    bool operator<(const Line& rhs) const
+    {
+        if (rhs.b != is_query)return m < rhs.m;
+        const Line* s = succ();
+        if (!s)return 0;
+        ll x = rhs.m;
+        return b - s->b < (s->m - m) * x;
+    }
+};
+
+struct HullDynamic : public multiset<Line>   // // will maintain lower hull for minimum
+{
+    bool bad(iterator y)
+    {
+        auto z = next(y);
+        if (y == begin())
+        {
+            if (z == end())return 0;
+            return y->m == z->m && y->b <= z->b;
+        }
+        auto x = prev(y);
+        if (z == end())return y->m == x->m && y->b <= x->b;
+        return (float128)(x->b - y->b)*(z->m - y->m) >= (float128)(y->b - z->b)*(y->m - x->m);
+    }
+    void add_line(ll m, ll b)
+    {
+        //auto y = insert({ -m, -b });// For minimum
+        auto y = insert({ m, b });// For maximum
+        y->succ = [=] { return next(y) == end() ? 0 : &*next(y); };
+        if (bad(y))
+        {
+            erase(y);
+            return;
+        }
+        while (next(y) != end() && bad(next(y)))erase(next(y));
+        while (y != begin() && bad(prev(y)))erase(prev(y));
+    }
+
+    ll getbest(ll x)
+    {
+        auto l = *lower_bound((Line)
+        {
+            x, is_query
+        });
+        //return -(l.m * x + l.b);//// For minimum
+        return (l.m * x + l.b);//// For maximum
+    }
+}st;
+vector<pair<pair<ll,ll>,ll>>v;
+int main()
+{
+    ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    ll n;
+    cin >> n;
+    for(int i=0;i<n;i++)
+    {
+        ll x,y,a;
+        cin >> x >> y >> a;
+        v.push_back({{x,y},a});
+    }
+    sort(v.begin(),v.end());
+    ll ans = 0;
+    st.add_line(0,0);
+    for(int i=0;i<n;i++)
+    {
+        ll x = v[i].first.first;
+        ll y = v[i].first.second;
+        ll a = v[i].second;
+
+        ll val = st.getbest(y)+x*y-a;
+        st.add_line(-x,val);
+        ans = max(ans,val);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
+
