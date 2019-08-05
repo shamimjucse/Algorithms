dfb223aed26ab10f7aff050722ee3ae408fd12c3
Shamim Imtiaz <shamimimtiaz9@gmail.com>
2019-08-05 17:45:09 +0600

    Update Euler Phi.cpp

--- Euler Phi.cpp
+++ Euler Phi.cpp
@@ -1,3 +1,22 @@
+ll phi[mx];
+void computeTotientPhi(int N)
+{
+    phi[1] = 1 ;
+    for(int i=2; i<=N; i++)
+    {
+        if(phi[i]==0)
+        {
+            phi[i] = i-1 ;
+            for(int j=i+i; j<=N; j+=i)
+            {
+                if(phi[j]==0)
+                    phi[j] = j ;
+                phi[j] = phi[j] - phi[j]/i ;
+            }
+        }
+    }
+}
+
 /******************************/
 for(int i=0; i<=MX; i++)
     phi[i]=i;
