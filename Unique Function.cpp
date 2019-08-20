0b4bc6e286d4f0e5c7bed50e9aac168d002f66c3
Shamim Imtiaz <shamimimtiaz9@gmail.com>
2019-08-20 18:06:58 +0600

    Update Unique Function.cpp

--- Unique Function.cpp
+++ Unique Function.cpp
@@ -1,17 +1,20 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <unordered_set>
+#include<bits/stdc++.h>
+using namespace std;
 
 int main()
 {
-	std::vector<int> v = { 5, 2, 1, 3, 4, 2, 2, 4, 5, 5 };
+	vector<int> v = { 5, 2, 1, 3, 4, 2, 2, 4, 5, 5 };
 
-	std::sort(v.begin(), v.end());
-	v.erase(std::unique(v.begin(), v.end()), v.end());
+	sort(v.begin(), v.end());
+	v.erase(unique(v.begin(), v.end()), v.end());
 
 	for (auto it = v.cbegin(); it != v.cend(); ++it)
-		std::cout << *it << ' ';
-
+		cout << *it << ' ';
 	return 0;
 }
+/*A const_iterator is an iterator that points to const content.
+  This iterator can be increased and decreased (unless it is itself
+  also const), just like the iterator returned by vector::begin,
+  but it cannot be used to modify the contents it points to, even
+  if the vector object is not itself const.
+*/
