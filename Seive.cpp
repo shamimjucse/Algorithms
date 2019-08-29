186b672bd93148eb8134e70a0b96f50a42994f51
Shamim Imtiaz <shamimimtiaz9@gmail.com>
2019-08-29 21:03:25 +0600

    Update Seive.cpp

--- Seive.cpp
+++ Seive.cpp
@@ -1,33 +1,63 @@
+// Light OJ - 1197
 #include<bits/stdc++.h>
 #define ll long long
-#define MX 1000000
 using namespace std;
 
-vector<ll>prime;
-bool vis[MX+2];//1 for prime
-void prime_seive()
+const int mx = 50000;
+vector<ll>pr;
+bitset<mx+2>vis;
+void seive()
 {
-    memset(vis,true,sizeof(vis));
-    vis[0]=vis[1]=false;
-    for(int i=4; i<=MX; i+=2) vis[i]=false;
-    for(int i=3; i*i<=MX; i+=2)
-        if(vis[i]) for(int j=i*i; j<=MX; j+=i+i) vis[j]=false;
-    prime.push_back(2);
-    for(int i=3;i<=MX;i+=2) if(vis[i]) prime.push_back(i);
+    vis[0]=vis[1]=1;
+    for(int i=4;i<=mx;i+=2)vis[i]=1;
+    for(int i=3;i*i<=mx;i+=2)
+    {
+        if(!vis[i])
+        {
+            for(int j=i*i;j<=mx;j+=i+i)
+            {
+                vis[j] = 1;
+            }
+        }
+    }
+    pr.push_back(2);
+    for(int i=3;i<=mx;i+=2)if(!vis[i])pr.push_back(i);
 }
-ll prime_chek(ll a)
+bool prime(ll n)
 {
-    prime_seive();
-    if(a==1) return 1;
-    else if(a==2) return 0;
-    else if(a%2==0) return 1;
-    else for(int i=1;i<prime.size()&&(prime[i]*prime[i]<=a);i++) if(a%prime[i]==0) return 1;
-    return 0;
+    if(n==1)return 0;
+    else if(n==2)return 1;
+    else if(n%2==0)return 0;
+    else for(int i=1;i<pr.size() && pr[i]*pr[i]<=n;i++)if(n%pr[i]==0)return 0;
+    return 1;
 }
-
+ll segmentseive(ll l, ll r)
+{
+    bool ck[r-l+2];
+    for(int i=0;i<=r-l;i++)ck[i] = 0;
+    if(l==1)ck[0] = 1;
+    for(int i=0;i<pr.size() && pr[i]*pr[i]<=r;i++)
+    {
+        ll st = pr[i] * ((l/pr[i])+(l%pr[i]!=0));
+        if(st==pr[i])st+=pr[i];
+        for(ll j=st;j<=r;j+=pr[i])ck[j-l] = 1;
+    }
+    ll res = 0;
+    for(int i=0;i<=r-l;i++)if(!ck[i])res++;
+    return res;
+}
 int main()
 {
-    prime_seive();
-    cout << prime.size() << endl;
+    seive();
+    //cout << pr.size() << endl;
+    int tc,t=1;
+    scanf("%d",&tc);
+    while(tc--)
+    {
+        ll l,r;
+        scanf("%lld %lld",&l,&r);
+        ll sm = segmentseive(l,r);
+        printf("Case %d: %lld\n",t++,sm);
+    }
     return 0;
 }
